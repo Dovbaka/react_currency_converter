6f1d4f80f5e71596422ae19f95892f173afc1ca1
Dovbaka <dovbaka99@gmail.com>
2020-10-08 14:52:45 +0300

    Updates

---
+++ precache-manifest.5c36b33cfaa6c90abb8b401a1ef80bad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf7b1d4d201dccb221ea33cfa8c1461",
+    "url": "/react_currency_converter/index.html"
+  },
+  {
+    "revision": "4582be0b67df42a0c35c",
+    "url": "/react_currency_converter/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fee7de0b4f6fd07f19bb",
+    "url": "/react_currency_converter/static/css/main.3c945440.chunk.css"
+  },
+  {
+    "revision": "4582be0b67df42a0c35c",
+    "url": "/react_currency_converter/static/js/2.7aca31cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_currency_converter/static/js/2.7aca31cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee7de0b4f6fd07f19bb",
+    "url": "/react_currency_converter/static/js/main.9ba4d8fc.chunk.js"
+  },
+  {
+    "revision": "65234ba98b0e25110afa",
+    "url": "/react_currency_converter/static/js/runtime-main.de975a25.js"
+  },
+  {
+    "revision": "b85858f57e4d137c22bda66aa34cc103",
+    "url": "/react_currency_converter/static/media/close.b85858f5.svg"
+  },
+  {
+    "revision": "84eea3d0dc3cee930c8c500bbb6eb151",
+    "url": "/react_currency_converter/static/media/edit.84eea3d0.svg"
+  },
+  {
+    "revision": "53634b8de6fba428ece8756acb06399c",
+    "url": "/react_currency_converter/static/media/save.53634b8d.svg"
+  },
+  {
+    "revision": "40a76008e52db6424f01f2988cba2653",
+    "url": "/react_currency_converter/static/media/swap.40a76008.svg"
+  }
+]);
